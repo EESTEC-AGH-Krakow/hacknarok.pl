0c765d20d9e64d2688f2877aa02c6ab43c75df3c
Konrad Polarczyk <konradpolarczyk98@gmail.com>
2022-12-28 18:46:54 +0100

    Some progress with the parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-const Background = styled.div<{offset: number}>`
+const Background = styled.div<{ offset: number }>`
   width: 100%;
   position: absolute;
-  top: calc(${props => props.offset * 0.6}px);
+  top: calc(${(props) => props.offset * 0.6}px);
   height: 500px;
   overflow-x: hidden;
   overflow-y: visible;
-`
+`;
 
-const Clouds = styled.img`
+const Clouds = styled.img<{ offset: number }>`
+  --offset-reduced: calc(${(props) => props.offset * 0.6}px);
   width: 100%;
   position: absolute;
-  min-width: 1300px;
-`
+  top: var(--offset-reduced);
+  min-width: 1600px;
+  height: calc(700px - var(--offset-reduced));
+`;
 
 const TitleContainer = styled.div`
   width: 100%;
@@ -22,80 +25,105 @@ const TitleContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding-top: 100px;
-`
+`;
 
 const Title = styled.h1`
   position: relative;
   color: black;
   font-family: Norse;
   font-size: 100px;
-`
-
+`;
 
 const MountainsContainer = styled.div`
-  position: absolute;
-  top: calc(50% - 150px);
+  position: relative;
+  overflow: hidden;
   width: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-  min-width: 1000px;
-  aspect-ratio: 3.305/1;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 //Screen blend mode makes the mountains nicely blend into the background
 const MountainsScreenLayer = styled.img`
   width: 100%;
-  position: absolute;
   mix-blend-mode: screen;
-`
+  min-width: 1600px;
+`;
 
 //Hard overlay so that there is no screening effect on the clouds
 const MountainsHardOverlay = styled.img`
-  width: 100%;
   position: absolute;
   mask-image: linear-gradient(180deg, black 20%, transparent 70%);
-`
+  width: 100%;
+  min-width: 1600px;
+`;
 
 const MountainBackdrop = styled.div`
-  position:absolute;
-  top: 20%;
-  height: 80%;
+  --height: 80%;
+  position: absolute;
   width: 100%;
+  height: var(--height);
+  top: calc(100% - var(--height));
   background-color: var(--background-color);
-`
+`;
 
 const FakeContent = styled.div`
-  width: 100%;
+  width: 600px;
   height: 200vh;
-`
+  background-color: aquamarine;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: red;
+`;
+
+const Page = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const ParallaxContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  overflow: hidden;
+`;
 
 function App() {
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
-      const onScroll = () => setOffset(window.pageYOffset);
-      // clean up code
-      window.removeEventListener('scroll', onScroll);
-      window.addEventListener('scroll', onScroll, { passive: true });
-      return () => window.removeEventListener('scroll', onScroll);
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
   return (
     <>
-      <Background offset={offset}>
-        <Clouds src="/clouds.jpg"/>
-        <TitleContainer>
-          <Title>Hacknarök</Title>
-        </TitleContainer>
-      </Background>
-      <MountainsContainer>
-        <MountainBackdrop/>
-        <MountainsScreenLayer src="/mountains_sum_overlay.png"/>
-        <MountainsHardOverlay src="/mountains_sum_overlay.png"/>
-      </MountainsContainer>
-      <FakeContent/>
+      <Page>
+        <ParallaxContainer>
+          <Clouds src="/clouds.jpg" offset={offset} />
+          <MountainsContainer>
+            <MountainBackdrop />
+            <MountainsScreenLayer src="/mountains_sum_overlay.png" />
+            <MountainsHardOverlay src="/mountains_sum_overlay.png" />
+          </MountainsContainer>
+        </ParallaxContainer>
+        <Content />
+      </Page>
+      <FakeContent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
