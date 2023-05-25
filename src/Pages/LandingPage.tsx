9631625794072ee96bb67d47f091a06c06c8f5e2
Konrad Polarczyk <47284299+Elgirhath@users.noreply.github.com>
2023-05-26 00:12:14 +0200

    Navbar (#2)
    * Fake commit
    
    * Working navbar

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,125 +1,124 @@
 import styled from "styled-components";
-import Navbar from "./Navbar";
 import ParallaxBackground from "./LandingPage/ParallaxBackground";
 
 const Content = styled.div`
-  position: relative;
-  margin-top: -80px;
-  width: 80%;
-  height: 160px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+    position: relative;
+    margin-top: -80px;
+    width: 80%;
+    height: 160px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 
-  @media only screen and (max-width: 600px) {
-    height: 450px;
-    flex-direction: column;
-    justify-content: flex-end;
-    margin-bottom: 50px;
-  }
+    @media only screen and (max-width: 600px) {
+        height: 450px;
+        flex-direction: column;
+        justify-content: flex-end;
+        margin-bottom: 50px;
+    }
 `;
 
 const ApplyContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 
-  @media only screen and (max-width: 600px) {
-    flex: 0;
-    margin: 20px;
-  }
+    @media only screen and (max-width: 600px) {
+        flex: 0;
+        margin: 20px;
+    }
 
-  span {
-    font-size: 18px;
-  }
+    span {
+        font-size: 18px;
+    }
 
-  button {
-    margin-top: 10px;
-    font-family: Norse;
-    font-size: 24px;
-    background-color: white;
-    color: black;
-    border-radius: 15px;
-    padding: 10px 30px 10px 30px;
-  }
+    button {
+        margin-top: 10px;
+        font-family: Norse;
+        font-size: 24px;
+        background-color: white;
+        color: black;
+        border-radius: 15px;
+        padding: 10px 30px 10px 30px;
+    }
 `;
 
 const EventPlaceContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 
-  @media only screen and (max-width: 600px) {
-    flex: 0;
-  }
+    @media only screen and (max-width: 600px) {
+        flex: 0;
+    }
 `;
 
 const EventPlaceInnerContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
 
-  h2 {
-    margin-top: 0;
+    h2 {
+        margin-top: 0;
 
-    @media only screen and (max-width: 600px) {
-      font-size: 20px;
-      margin: 0;
+        @media only screen and (max-width: 600px) {
+            font-size: 20px;
+            margin: 0;
+        }
     }
-  }
 `;
 
 const LogoContainer = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: flex-end;
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
 
-  @media only screen and (max-width: 600px) {
-    flex: 0;
-  }
+    @media only screen and (max-width: 600px) {
+        flex: 0;
+    }
 `;
 
 const Logo = styled.img`
-  width: 200px;
-  margin-top: -20px;
+    width: 200px;
+    margin-top: -20px;
 `;
 
 const Page = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
 `;
 
 function LandingPage() {
-  return (
-    <Page>
-      <ParallaxBackground />
-      <Content>
-        <EventPlaceContainer>
-          <EventPlaceInnerContainer>
-            <h2>23-24 marca</h2>
-            <span>Krakowski Park Technologiczny</span>
-          </EventPlaceInnerContainer>
-        </EventPlaceContainer>
-        <ApplyContainer>
-          <span>Zapisy do 17.01</span>
-          <button>Zapisz się</button>
-        </ApplyContainer>
-        <LogoContainer>
-          <Logo src="/logo_eestec.png" />
-        </LogoContainer>
-      </Content>
-    </Page>
-  );
+    return (
+        <Page>
+            <ParallaxBackground />
+            <Content>
+                <EventPlaceContainer>
+                    <EventPlaceInnerContainer>
+                        <h2>23-24 marca</h2>
+                        <span>Krakowski Park Technologiczny</span>
+                    </EventPlaceInnerContainer>
+                </EventPlaceContainer>
+                <ApplyContainer>
+                    <span>Zapisy do 17.01</span>
+                    <button>Zapisz się</button>
+                </ApplyContainer>
+                <LogoContainer>
+                    <Logo src="/logo_eestec.png" />
+                </LogoContainer>
+            </Content>
+        </Page>
+    );
 }
 
 export default LandingPage;
--- src/Pages/Navbar.tsx
+++ src/Pages/Navbar.tsx
@@ -8,117 +8,178 @@ import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import * as React from "react";
 import { useState, RefObject } from "react";
 import styled from "styled-components";
+import { css } from "styled-components";
+import useIsIntersecting from "../hooks/useIsInViewport";
 
-const TabButton = styled.button<{ isActive: boolean }>`
-  background-color: ${(props) => (props.isActive ? "white" : "transparent")};
-  color: black;
-  text-transform: lowercase;
-  font-family: Montserrat;
-  border-radius: 100px;
-  font-size: 16px;
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-left: 5px;
+const TabButtonBase = css`
+    box-sizing: border-box;
+    text-transform: lowercase;
+    font-family: Montserrat;
+    border-radius: 100px;
+    font-size: 16px;
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-left: 5px;
+    outline: inherit;
+    border: 1px solid transparent;
 
-  border-color: transparent;
+    :hover {
+        background-color: rgba(180, 180, 180, 0.932);
+        border-color: transparent;
+        outline: inherit;
+    }
 
-  :hover {
-    background-color: rgba(0, 0, 0, 0.2);
-  }
+    //display outline only if navigated with keyboard
+    :focus:not(:focus-visible) {
+        outline: none;
+    }
 
-  @media screen and (max-width: 600px) {
-    display: none;
-  }
+    @media screen and (max-width: 600px) {
+        display: none;
+    }
+`;
+
+const TabButtonOnTopOfPage = css<{ isActive: boolean }>`
+    ${TabButtonBase}
+    background-color: ${(props) => (props.isActive ? "white" : "transparent")};
+
+    color: black;
+`;
+
+const TabButtonScrolled = css<{ isActive: boolean }>`
+    ${TabButtonBase}
+    background-color: ${(props) => (!props.isActive ? "#13131397" : "white")};
+
+    border: 1px solid;
+    border-color: ${(props) => (!props.isActive ? "white" : "black")};
+
+    color: ${(props) => (!props.isActive ? "white" : "black")};
+`;
+
+const TabButton = styled.button<{
+    isActive: boolean;
+    isOnTopOfThePage: boolean;
+}>`
+    ${TabButtonBase}
+
+    ${(props) =>
+        props.isOnTopOfThePage ? TabButtonOnTopOfPage : TabButtonScrolled}
+
+    transition: background-color 0.4s, border 0.4s, color 0.4s;
 `;
 
 const Bar = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  margin-top: 20px;
-  margin-right: 30px;
-  z-index: 1;
-  display: flex;
-  flex-direction: row;
+    position: fixed;
+    top: 0;
+    right: 0;
+    margin-top: 20px;
+    margin-right: 30px;
+    z-index: 1;
+    display: flex;
+    flex-direction: row;
 `;
 
 const TabsContainer = styled.div``;
 const MenuButtonContainer = styled.div`
-  display: none;
-  @media screen and (max-width: 600px) {
-    display: block;
-  }
+    display: none;
+    @media screen and (max-width: 600px) {
+        display: block;
+    }
 `;
 
 interface NavbarProps {
-  pageRefs: Record<string, RefObject<HTMLInputElement>>;
+    pageRefs: Record<string, RefObject<HTMLInputElement>>;
+    currentPage: string;
+    onPageSelected: (pageName: string) => void;
 }
 
-export default function Navbar({ pageRefs }: NavbarProps) {
-  const [selectedPage, setSelectedPage] = useState(Object.keys(pageRefs)[0]);
+export default function Navbar({
+    pageRefs,
+    currentPage,
+    onPageSelected,
+}: NavbarProps) {
+    const [state, setState] = React.useState(false);
 
-  const [state, setState] = React.useState(false);
+    const [isOnTopOfThePage, setIsOnTopOfThePage] = useState(true);
 
-  const selectPage = (pageName: string) => {
-    pageRefs[pageName].current?.scrollIntoView({
-      block: "start",
-      behavior: "smooth",
+    const selectPage = (pageName: string) => {
+        pageRefs[pageName].current?.scrollIntoView({
+            block: "start",
+            behavior: "smooth",
+        });
+
+        onPageSelected(pageName);
+    };
+
+    React.useEffect(() => {
+        window.onscroll = () => {
+            if (window.scrollY > 500 && isOnTopOfThePage) {
+                setIsOnTopOfThePage(false);
+            }
+            if (window.scrollY <= 500 && !isOnTopOfThePage) {
+                setIsOnTopOfThePage(true);
+            }
+        };
+
+        return () => {
+            window.onscroll = null;
+        };
     });
 
-    setSelectedPage(pageName);
-  };
+    const toggleDrawer = (newState: boolean) => {
+        if (state == newState) return;
+        setState(newState);
+    };
 
-  const toggleDrawer = (newState: boolean) => {
-    if (state == newState) return;
-    setState(newState);
-  };
-
-  return (
-    <>
-      <Bar>
-        <TabsContainer>
-          {Object.keys(pageRefs).map((p) => (
-            <TabButton
-              isActive={selectedPage === p}
-              onClick={() => selectPage(p)}
+    return (
+        <>
+            <Bar>
+                <TabsContainer>
+                    {Object.keys(pageRefs).map((p) => (
+                        <TabButton
+                            isActive={currentPage === p}
+                            isOnTopOfThePage={isOnTopOfThePage}
+                            onClick={() => selectPage(p)}
+                        >
+                            {p}
+                        </TabButton>
+                    ))}
+                </TabsContainer>
+                <MenuButtonContainer>
+                    <IconButton
+                        aria-label="menu"
+                        color="default"
+                        onClick={() => setState(true)}
+                    >
+                        <MenuIcon style={{ fontSize: 30 }} />
+                    </IconButton>
+                </MenuButtonContainer>
+            </Bar>
+            <SwipeableDrawer
+                anchor="right"
+                open={state}
+                onOpen={() => toggleDrawer(true)}
+                onClose={() => toggleDrawer(false)}
             >
-              {p}
-            </TabButton>
-          ))}
-        </TabsContainer>
-        <MenuButtonContainer>
-          <IconButton
-            aria-label="menu"
-            color="default"
-            onClick={() => setState(true)}
-          >
-            <MenuIcon style={{ fontSize: 30 }} />
-          </IconButton>
-        </MenuButtonContainer>
-      </Bar>
-      <SwipeableDrawer
-        anchor="right"
-        open={state}
-        onOpen={() => toggleDrawer(true)}
-        onClose={() => toggleDrawer(false)}
-      >
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => toggleDrawer(false)}
-          onKeyDown={() => toggleDrawer(false)}
-        >
-          <List>
-            {Object.keys(pageRefs).map((pageName, index) => (
-              <ListItem key={pageName}>
-                <ListItemButton onClick={() => selectPage(pageName)}>
-                  <ListItemText primary={pageName} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </SwipeableDrawer>
-    </>
-  );
+                <Box
+                    sx={{ width: 250 }}
+                    role="presentation"
+                    onClick={() => toggleDrawer(false)}
+                    onKeyDown={() => toggleDrawer(false)}
+                >
+                    <List>
+                        {Object.keys(pageRefs).map((pageName, index) => (
+                            <ListItem key={pageName}>
+                                <ListItemButton
+                                    onClick={() => selectPage(pageName)}
+                                >
+                                    <ListItemText primary={pageName} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </SwipeableDrawer>
+        </>
+    );
 }
