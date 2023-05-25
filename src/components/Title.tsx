e13ce5c27ad16200e29e3d9e90bf9ca66f3b779f
Konrad Polarczyk <47284299+Elgirhath@users.noreply.github.com>
2023-05-26 00:37:09 +0200

    About done (#3)

---
+++ src/components/Title.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    color: black;
+    margin-bottom: 60px;
+    font-family: Norse;
+    font-size: 60px;
+
+    @media only screen and (max-width: 600px) {
+        font-size: 40px;
+    }
+`;
