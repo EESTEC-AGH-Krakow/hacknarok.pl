daaca3a9c30d55cce0174b4f8005dca425d62551
patry77 <patry28011@gmail.com>
2024-04-20 12:18:40 +0200

    Update before_deploy.sh

--- iac/before_deploy.sh
+++ iac/before_deploy.sh
@@ -7,7 +7,7 @@ printf "\nstart the ssh-agent to run ssh commands ...\n"
 eval "$(ssh-agent -s)"
 
 printf "\nchange permissions on decrypted file to avoid warnings ...\n"
-chmod 600 ./.deploy_key
+chmod 600 ./.deploy_key.enc
 
 printf "\ncreate ssh directory and avoid GitHub asking if we want to \"check\" the identity of target host (VPS) ...\n"
 mkdir -p ~/.ssh
@@ -16,9 +16,9 @@ chmod 700 ~/.ssh
 chmod 600 ~/.ssh/config
 
 printf "\nadd the decrypted file as preferred ssh RSA key ...\n"
-ssh-add ./.deploy_key
+ssh-add ./.deploy_key.enc
 
 printf "\nrun the pwd command on the deployment server using the decrypted file as \"identity\" file (RSA Key) ...\n"
-ssh -i ./.deploy_key "$USERNAME"@"$SERVER_ADDRESS" pwd
+ssh -i ./.deploy_key.enc "$USERNAME"@"$SERVER_ADDRESS" pwd
 
 printf "\nconnectivity test has been completed successfully!\n"
