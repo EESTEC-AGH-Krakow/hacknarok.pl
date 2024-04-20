981393e5f3b8c8e68c6ca0e560d902a8ed187035
patry77 <patry28011@gmail.com>
2024-04-20 11:24:35 +0200

    DODANIE ZMIENNYCH

--- iac/deploy.sh
+++ iac/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 LOCATION=$1
+USERNAME=$2
+SERVER_ADDRESS=$3
 
 printf "sync files and remove unnecessary ...\n"
 rsync -Pavh -e 'ssh -i ./.deploy_key' ./dist "$USERNAME"@"$SERVER_ADDRESS":~"$LOCATION" --delete-after
