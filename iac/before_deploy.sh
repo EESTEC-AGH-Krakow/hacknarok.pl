2f405d2c6c07fec273abc6f7f2d2166a40caee35
patry77 <patry28011@gmail.com>
2024-04-20 12:16:31 +0200

    Update before_deploy.sh

--- iac/before_deploy.sh
+++ iac/before_deploy.sh
@@ -3,22 +3,22 @@
 USERNAME=$1
 SERVER_ADDRESS=$2
 
-printf "start decrypting file ...\n"
-openssl aes-256-cbc -K $encrypted_189e52c2c347_key -iv $encrypted_189e52c2c347_iv -in ./iac/deploy_key.enc -out ./.deploy_key -d
-
 printf "\nstart the ssh-agent to run ssh commands ...\n"
 eval "$(ssh-agent -s)"
 
 printf "\nchange permissions on decrypted file to avoid warnings ...\n"
 chmod 600 ./.deploy_key
 
-printf "\navoid Travis asking if we want to \"check\" the identity of target host (VPS) ...\n"
+printf "\ncreate ssh directory and avoid GitHub asking if we want to \"check\" the identity of target host (VPS) ...\n"
+mkdir -p ~/.ssh
 echo -e "Host $SERVER_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/config
 
-printf "\nadd the decrpyted file as preferred ssh RSA key ...\n"
+printf "\nadd the decrypted file as preferred ssh RSA key ...\n"
 ssh-add ./.deploy_key
 
 printf "\nrun the pwd command on the deployment server using the decrypted file as \"identity\" file (RSA Key) ...\n"
 ssh -i ./.deploy_key "$USERNAME"@"$SERVER_ADDRESS" pwd
 
-printf "\nconnectivity test has been completed successfully!"
+printf "\nconnectivity test has been completed successfully!\n"
