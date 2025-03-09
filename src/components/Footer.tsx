cffde3388c39d99999546fd729300f5996fe6e9a
Piotr Postek <125039363+Piotr-Postek@users.noreply.github.com>
2025-03-09 20:15:29 +0100

    zmiana nazwy regulaminu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
                     <MailtoLink
                         target="_blank"
                         rel="noopener noreferrer"
-                        href="regulamin-hack9.pdf"
+                        href="regulamin_hack_9.pdf"
                     >
                         regulamin
                     </MailtoLink>
